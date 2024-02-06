84bf06e1298413d2f0e03821067389e3ee9e647e
shrekoyya <shrennivasraw@gmail.com>
2024-02-06 23:14:23 +0530

    Added the code for Api

---
+++ ApiSolution/Controller/LocationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ApiSolution.Model;
+
+namespace ApiSolution.Controller
+{
+    public class LocationController : ApiController
+    {
+
+        public IEnumerable<Locations> GetAllusers()
+        {
+            return location;
+        }
+        Locations[] location = new Locations[]
+       {
+
+        new Locations { locationID = 1, sartdate = DateTime.Today.AddHours(10),enddate=DateTime.Today.AddHours(13)},
+        new Locations { locationID = 2, sartdate = DateTime.Today.AddHours(13),enddate=DateTime.Today.AddHours(18) },
+        new Locations { locationID = 3, sartdate = DateTime.Today.AddHours(20),enddate=DateTime.Today.AddHours(24) },
+       };
+        // GET api/<controller>/5
+        public IHttpActionResult Getuses()
+        {
+
+            var locations = location.FirstOrDefault(p => p.sartdate == DateTime.Today.AddHours(10) && p.enddate == DateTime.Today.AddHours(13));
+            if (locations == null)
+            {
+                return NotFound();
+            }
+            return Ok(locations);
+        }
+
+    }
+}
---
+++ ApiSolution/Model/Locations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiSolution.Model
+{
+    public class Locations
+    {
+        public int locationID { get; set; }
+        public DateTime sartdate { get; set; }
+        public DateTime enddate { get; set; }
+    }
+}
